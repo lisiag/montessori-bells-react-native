b865ebaad6dae3646ac64201af0e4283eecc54e8
Lisia Grocott <lisiagrocott@gmail.com>
2020-09-12 21:16:30 +1200

    wip - get rid of tabs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Button } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./ui/hooks/useCachedResources";
 import useColorScheme from "./ui/hooks/useColorScheme";
 import Navigation from "./ui/navigation";
+
+import "react-native-gesture-handler";
+
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./ui/screens/HomeScreen";
+import AboutScreen from "./ui/screens/AboutScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import Match from "./ui/screens/Match";
+
+const Stack = createStackNavigator();
 
 export default function App() {
     const isLoadingComplete = useCachedResources();
@@ -14,10 +25,27 @@ export default function App() {
         return null;
     } else {
         return (
-            <SafeAreaProvider>
-                <Navigation colorScheme={colorScheme} />
-                <StatusBar />
-            </SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Home"
+                        component={HomeScreen}
+                        options={{
+                            title: "Welcome",
+                            headerRight: () => (
+                                <Button
+                                    onPress={() => alert("This is a button!")}
+                                    title="Login"
+                                    color="#000"
+                                />
+                            ),
+                        }}
+                    />
+                    <Stack.Screen name="About" component={AboutScreen} />
+                    <Stack.Screen name="Match" component={Match} />
+                </Stack.Navigator>
+                {/* Rest of your app code */}
+            </NavigationContainer>
         );
     }
 }
